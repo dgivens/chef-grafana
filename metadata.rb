848ac710832651864ab35a131b9af4ac8c880255
Daniel Givens <daniel@givens.io>
2015-02-11 17:44:33 -0600

    Version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'daniel.givens@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures grafana'
 long_description 'Installs/Configures grafana'
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'ark'
