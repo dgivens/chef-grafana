1aaa266887850a58235f066169b7fee4dcf58ac0
Daniel Givens <daniel@givens.io>
2015-02-11 09:29:58 -0600

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,21 @@
+default['grafana']['file']['type'] = 'tar.gz' # tar.gz | zip
+default['grafana']['file']['version'] = '1.9.1'
+default['grafana']['file']['url'] = 'http://grafanarel.s3.amazonaws.com/grafana-%{version}.%{type}'
+default['grafana']['file']['checksum'] = 'c328c7a002622f672affbcaabd5e64ae279be1051ee27c62ba22bfed63680508' # sha256 ( shasum -a 256 FILENAME )
+default['grafana']['install_base'] = '/opt'
+default['grafana']['install_path'] = '/opt/grafana'
+default['grafana']['admin_password'] = ''
+
+default['grafana']['elasticsearch_url'] = '"http://"+window.location.hostname+":9200"'
+default['grafana']['datasources'] = {
+  'graphite' => {
+    'type' => "'graphite'",
+    'url' => '"http://"+window.location.hostname+":8080""'
+  },
+  'elasticsearch' => {
+    'type' => "'elasticsearch'",
+    'url' => '"http://"+window.location.hostname+":9200"',
+    'index' => "'grafana-dash'",
+    'grafanaDB' => 'true'
+  }
+}
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'grafana'
+maintainer       'Rackspace, Inc.'
+maintainer_email 'daniel.givens@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures grafana'
+long_description 'Installs/Configures grafana'
+version          '0.1.0'
+
+depends          'ark'
